ac755dbce48bf608321bec6f1ae89a4cf6b2b5dd
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2017-05-22 21:47:43 -0500

    added images for assets

--- Miracle Pills/ViewController.swift
+++ Miracle Pills/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     //test
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad()        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
