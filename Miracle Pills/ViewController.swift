96781231fde168e69cea725cbce8e7269f28e38a
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2017-05-24 22:59:55 -0500

    populated state picker

--- Miracle Pills/ViewController.swift
+++ Miracle Pills/ViewController.swift
@@ -8,15 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
     @IBOutlet weak var statePicker: UIPickerView!
     //test
     @IBOutlet weak var statePickerBtn: UIButton!
     
+    let states = ["Alaska", "Arkansas", "Alabama","California","Maine","New York"]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        statePicker.dataSource = self
+        statePicker.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -26,6 +30,18 @@ class ViewController: UIViewController {
     }
 
     @IBAction func stateBtnPressed(_ sender: Any) {
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
     }
 
 }
